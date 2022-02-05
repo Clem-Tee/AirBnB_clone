2aced439d75265c0d1fc2c43f898cad63c7996a6
Clem-Tee <clementogol@gmail.com>
2022-02-05 22:49:01 +0300

    Updated the commmand interpreter

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ import models
 
 
 class HBNBCommand(cmd.Cmd):
-    """ HBNB console """
+    """HBNB console"""
     prompt = '(hbnb)'
 
     def do_EOF(self, arg):
@@ -14,12 +14,31 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-	""" overwriting the emptyline method """
+	"""Overwriting the emptyline method"""
 	return False
 
     def do_quit(self, arg):
 	"""Quit command to exit the program"""
 	return True
+
+    def do_create(self, arg):
+	"""Creates a new instance of a class"""
+	if len(args) == 0:
+	    print("** class name missing **")
+	    return False
+	if args[0] in classes:
+	    instance = classes[args[0]]()
+	else:
+	    print("**Class doesn't exist**")
+	    return False
+	print(instance.id)
+	instance.save()
+
+    def do_show(self, arg):
+    """Prints an instance as string based on the class and id"""
+	    if len(args) == 0;
+	        print("** class name missing **")
+		retun False
 
 
 if __name__ == '__main__':
